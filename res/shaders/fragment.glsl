fa23d477017bc3c0422a30be764418991798e1f3
Crosslywere <ogboruj@outlook.com>
2025-02-23 02:20:09 +0100

    removed unused code

--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -1,12 +1,5 @@
 #version 330 core
 in vec2 iTexCoord;
-
-float quantizeAdjustment(vec3 color) {
-    float grayscale = max(color.r, max(color.g, color.b));
-    return grayscale;
-}
-
 void main() {
-
     gl_FragColor = vec4(iTexCoord, 0., 1.);
 }
